8a779d89652ecaca47598e35ee02e5592181c072
Aira Jena <airajena.hack@gmail.com>
2025-01-07 22:05:18 +0530

    feat: Publish message to RabbitMQ

---
+++ reviewms/src/main/java/com/_paradigms/reviewms/review/messaging/ReviewMessageProducer.java
@@ -0,0 +1,24 @@
+package com._paradigms.reviewms.review.messaging;
+
+import com._paradigms.reviewms.review.Review;
+import com._paradigms.reviewms.review.dto.ReviewMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class ReviewMessageProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ReviewMessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Review review){
+        ReviewMessage reviewMessage = new ReviewMessage();
+        reviewMessage.setId(reviewMessage.getId());
+        reviewMessage.setTitle(review.getTitle());
+        reviewMessage.setDescription(review.getDescription());
+        reviewMessage.setRating(review.getRating());
+        reviewMessage.setCompanyId(review.getCompanyId());
+        rabbitTemplate.convertAndSend("companyRatingQueue", reviewMessage);
+
+    }
+}
