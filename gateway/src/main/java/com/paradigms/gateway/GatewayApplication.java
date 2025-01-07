16dfeb2fc7cb1f2781a91889d7d5dd516a64b433
Aira Jena <airajena.hack@gmail.com>
2025-01-07 11:10:54 +0530

    fix: Fix API gateway path

--- gateway/src/main/java/com/paradigms/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/paradigms/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com._paradigms.gateway;
+package com.paradigms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gateway/src/test/java/com/paradigms/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/paradigms/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com._paradigms.gateway;
+package com.paradigms.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
