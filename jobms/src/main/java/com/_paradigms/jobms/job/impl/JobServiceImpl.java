9e7588d669e0445b8983e2f0426a7565cfd4b25c
Aira Jena <airajena.hack@gmail.com>
2025-01-07 15:52:41 +0530

    feat: Integrate and set up Resilience4J

--- jobms/src/main/java/com/_paradigms/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/_paradigms/jobms/job/impl/JobServiceImpl.java
@@ -10,6 +10,7 @@ import com._paradigms.jobms.job.dto.JobDTO;
 import com._paradigms.jobms.job.external.Company;
 import com._paradigms.jobms.job.external.Review;
 import com._paradigms.jobms.job.mapper.JobMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -42,6 +43,7 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
+    @CircuitBreaker(name = "companyBreaker")
     public List<JobDTO> findAll() {
         List<Job> jobs = jobRepository.findAll();
         List<JobDTO> jobDTOS = new ArrayList<>();
