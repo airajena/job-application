f7b1377c548533fee8a10e9a380f009dcb686bf4
Aira Jena <airajena.hack@gmail.com>
2025-02-06 20:35:11 +0530

    feat: Add docker configurations to company service

--- companyms/src/main/java/com/_paradigms/companyms/company/clients/ReviewClient.java
+++ companyms/src/main/java/com/_paradigms/companyms/company/clients/ReviewClient.java
@@ -3,7 +3,7 @@ package com._paradigms.companyms.company.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-@FeignClient("REVIEWMS")
+@FeignClient(name= "REVIEWMS", url = "${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews/averageRating")
     Double getAverageRatingForCompany(
