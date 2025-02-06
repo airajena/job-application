106e4e02aa5f38d8f0b24abc1074015b8a4f924b
Aira Jena <airajena.hack@gmail.com>
2025-02-06 19:58:04 +0530

    refactor: Removed Job Monolithic application

--- jobapp/src/main/java/com/_paradigms/jobapp/company/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package com._paradigms.jobapp.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
